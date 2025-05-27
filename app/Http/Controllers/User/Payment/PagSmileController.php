b6e1e57c21b9b47bec88829e1dcd9a848e5e9389
SamoelDuarte <samoelduarte@betasolucao.com.br>
2025-05-27 20:21:38 -0300

    Update PagSmileController.php

--- app/Http/Controllers/User/Payment/PagSmileController.php
+++ app/Http/Controllers/User/Payment/PagSmileController.php
@@ -47,7 +47,7 @@ class PagSmileController extends Controller
         $order = Common::saveOrder($request->all(), $txnId, $chargeId, 'Pending', 'online', $user->id);
         $order_id = $order->id;
         Common::saveOrderedItems($order->id);
-
+dd(route('customer.itemcheckout.pagSmile.notify',getParam()));
         // Payload para PagSmile
         $payload = [
             'app_id'            => $app_id,
